e691f4aae760a8dc6fa8a96d9e032ba04b520ee3
Hartmann, Eric <coguy450@gmail.com>
2017-12-22 17:08:16 -0700

    added more orders

--- orderBook.js
+++ orderBook.js
@@ -14,31 +14,19 @@ exports.newWSConnection = (message, ws) => {
 
 function startAsks(market, thisWS) {
   bit.market(market).on('askUpdate', (data) => {
-    let lastAskUpdate = {asks: data.asks.top(10), market: market}
+    let lastAskUpdate = {asks: data.asks.top(15), market: market}
     if (thisWS.readyState === 1) {
       thisWS.send(JSON.stringify(lastAskUpdate))
     } else {
       thisWS.terminate()
     }
-  });
+  })
 }
 
-// function newTicker(market, socket) {
-//   bittrex.websockets.client(function() {
-//     bittrex.websockets.subscribe([market], function(data) {
-//       console.log(data)
-//       data.A.forEach(function(data_for) {
-//         data_for.Deltas.forEach(function(marketsDelta) {
-//           console.log('Ticker Update for '+ marketsDelta.MarketName, marketsDelta);
-//         })
-//       })
-//     })
-//   })
-// }
 
 function startBids(market, thisWS) {
   bit.market(market).on('bidUpdate', (data) => {
-    let lastBidUpdate = {bids: data.bids.top(10), market: market}
+    let lastBidUpdate = {bids: data.bids.top(15), market: market}
     if (thisWS.readyState === 1) {
       thisWS.send(JSON.stringify(lastBidUpdate))
     } else {
